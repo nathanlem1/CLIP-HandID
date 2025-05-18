3d27c601bb461c493d542437a15917aa59b82006
Nathanael L. Baisa <nathanaellmss@gmail.com>
2025-05-18 21:33:13 +0100

    Adjusting batch_size for RN50 and ViT-B/16 differently for better performance

--- train_finetune.py
+++ train_finetune.py
@@ -227,7 +227,7 @@ def main():
     parser.add_argument('--m_name', default='clip_hand_vit', type=str,
                         help='Output model name - clip_hand_vit OR clip_hand_rn50.')
     parser.add_argument('--train_all', action='store_true', help='use all training data')
-    parser.add_argument('--batch_size', default=4, type=int, help='batch_size')  # 4, 10, 20, 32, etc
+    parser.add_argument('--batch_size', default=20, type=int, help='batch_size')  # 4, 10, 20, 32, etc
     parser.add_argument('--num_workers', default=0, type=int,
                         help='Number of workers to use: 0, 8, etc. Setting to 8 workers may run faster.')
     parser.add_argument('--color_jitter', action='store_true', default=False, help='use color jitter in training')
@@ -252,6 +252,10 @@ def main():
     parser.add_argument('--stride_size', type=tuple, default=(16, 16), help='Stride size for creating image patches')
 
     args = parser.parse_args()
+
+    # Adjust batch size
+    if args.backbone_name == 'RN50':
+        args.batch_size = 4  # For better performance: batch_size=4 for CLIP RN50 & batch_size=20 for ViT-B/16.
 
     # For reproducibility
     if args.is_repr:
